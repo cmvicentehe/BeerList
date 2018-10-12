ec0c88171211469ea95227cddded845a3980b5c8
Carlos M. Vicente Herrero <cmvicentehe@gmail.com>
2018-10-12 11:40:25 +0200

    Initial Commit

---
+++ BeerList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerList
+//
+//  Created by Carlos Manuel Vicente Herrero on 12/10/2018.
+//  Copyright © 2018 Carlos Manuel Vicente Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
